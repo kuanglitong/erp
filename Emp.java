fe98f34f99929cf5a93bf9e8bc106fe3ecaa3faa
klt <1405647590@qq.com>
2021-05-24 17:42:40 +0800

    Change Emp.java File

--- Emp.java
+++ Emp.java
@@ -1,5 +1,3 @@
-public class Hello {
-	public static void main(String args[]) {
-		System.out.println("Hello GIT！");
-	}
+public class Emp implements java.io.Serializable {
+	private Integer empno;
 }
