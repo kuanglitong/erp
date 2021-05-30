ec539485d8d5d3eb360bf7e6ee30194ab5c72e33
klt <1405647590@qq.com>
2021-05-30 09:58:33 +0800

    Change Hello.java in dev branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,7 @@
+import java.util.*
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("Hello GIT！");
-		System.out.println("修改仓库文件1");
-		System.out.println("修改仓库文件2");	
-		System.out.println("试探回退操作");
-		System.out.println(new java.util.Date());						
+		System.out.println("解决冲突");
+		Date date = new Date();
 	}
 }
