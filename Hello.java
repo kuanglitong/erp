78b2b7c684647991fad81135c67254b06f7e5c4b
klt <1405647590@qq.com>
2021-05-30 11:08:16 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 import java.util.*
 public class Hello {
 	public static void main(String args[]) {
+		System.out.println("解决冲突3");
+		System.out.println("解决冲突3");
 		System.out.println("解决冲突2");
 		System.out.println("解决冲突2");
 		System.out.println("解决冲突");
